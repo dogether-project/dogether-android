7ee7f541f53e1df2e7751de000ef03ee1260adbe
jangjh123 <gn0001231@gmail.com>
2025-07-24 23:27:14 +0900

    chore: 모듈 생성 및 의존성 추가
    Ref: #1

---
+++ common/src/main/java/site/dogether/common/MyClass.kt
@@ -0,0 +1,4 @@
+package site.dogether.common
+
+class MyClass {
+}
--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,28 +1,26 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
 }
 
 android {
-    namespace = "site.dogether.android"
+    namespace = "site.dogether.data"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "site.dogether.android"
         minSdk = 26
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
-    buildFeatures {
-        compose = true
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -36,17 +34,9 @@ kotlin {
 }
 
 dependencies {
-    implementation(project(":presentation"))
     implementation(project(":domain"))
-    implementation(project(":data"))
     implementation(project(":common"))
 
-    // Compose BOM
-    implementation(platform(libs.androidx.compose.bom))
-
-    // Compose core
-    implementation(libs.bundles.compose.core)
-
-    // Koin
-    implementation(libs.bundles.koin)
+    // Ktor
+    implementation(libs.bundles.ktor)
 }
