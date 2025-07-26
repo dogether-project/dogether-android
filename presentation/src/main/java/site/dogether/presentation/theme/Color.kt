4a9688cb7b94b06c54e4b5022d62fb8fcb2569a0
jangjh123 <gn0001231@gmail.com>
2025-07-27 02:50:24 +0900

    chore: 색상 추가
    Related to: #2

---
+++ presentation/src/main/java/site/dogether/presentation/theme/Color.kt
@@ -0,0 +1,31 @@
+package site.dogether.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Grey0 = Color(0xFFFFFFFF)
+val Grey50 = Color(0xFFF7F7FC)
+val Grey100 = Color(0xFFD2D4DB)
+val Grey200 = Color(0xFFB3B5BC)
+val Grey300 = Color(0xFF9496A2)
+val Grey400 = Color(0xFF777986)
+val Grey500 = Color(0xFF515151)
+val Grey600 = Color(0xFF3B3B3B)
+val Grey700 = Color(0xFF2B2B2B)
+val Grey800 = Color(0xFF1F1F1F)
+val Grey900 = Color(0xFF101010)
+
+val Blue100 = Color(0xFFF3F7FA)
+val Blue200 = Color(0xFF56A5FF)
+val Blue300 = Color(0xFF56A5FF)
+val Blue400 = Color(0xFF3E8AE1)
+
+val Red = Color(0xFFFB5171)
+val Yellow = Color(0xFFEDD16F)
