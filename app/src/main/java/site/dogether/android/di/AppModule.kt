83253665451893baf84da04f2bf266a4dcc75329
jangjh123 <gn0001231@gmail.com>
2025-07-26 22:44:27 +0900

    feat: CoroutineDispatcher 의존성 주입
    Related to: #1

--- app/src/main/java/site/dogether/android/di/AppModule.kt
+++ app/src/main/java/site/dogether/android/di/AppModule.kt
@@ -5,5 +5,6 @@ val appModule = listOf(
     useCaseModule,
     repositoryModule,
     localDataSourceModule,
-    networkModule
+    networkModule,
+    dispatcherModule
 )
--- common/src/main/java/site/dogether/common/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package site.dogether.common
-
-class MyClass {
-}
