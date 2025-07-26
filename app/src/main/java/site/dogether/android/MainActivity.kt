9df4a14c9c392a540ce09c9246acfdc8546e054b
jangjh123 <gn0001231@gmail.com>
2025-07-27 02:50:24 +0900

    chore: 색상 추가
    Refs: #2

--- app/src/main/java/site/dogether/android/MainActivity.kt
+++ app/src/main/java/site/dogether/android/MainActivity.kt
@@ -3,7 +3,7 @@ package site.dogether.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import site.dogether.android.ui.theme.DogetherAndroidTheme
+import site.dogether.presentation.theme.DogetherAndroidTheme
 import site.dogether.presentation.AppNavGraph
 
 class MainActivity : ComponentActivity() {
--- presentation/src/main/java/site/dogether/presentation/theme/Type.kt
+++ presentation/src/main/java/site/dogether/presentation/theme/Type.kt
@@ -1,4 +1,4 @@
-package site.dogether.android.ui.theme
+package site.dogether.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
