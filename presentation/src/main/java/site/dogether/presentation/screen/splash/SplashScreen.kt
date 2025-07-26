d32fbd6b06e9287722893cdfae82f9c12d38820b
jangjh123 <gn0001231@gmail.com>
2025-07-27 03:11:33 +0900

    feat: 스플래쉬 UI 구현
    Related to: #2

--- presentation/src/main/java/site/dogether/presentation/screen/splash/SplashScreen.kt
+++ presentation/src/main/java/site/dogether/presentation/screen/splash/SplashScreen.kt
@@ -1,13 +1,21 @@
 package site.dogether.presentation.screen.splash
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import site.dogether.presentation.R
 
 @Composable
 fun SplashScreen() {
     Box(modifier = Modifier.fillMaxSize()) {
-
+        Image(
+            modifier = Modifier.align(Alignment.Center),
+            painter = painterResource(R.drawable.ic_splash),
+            contentDescription = "icon_splash"
+        )
     }
 }
