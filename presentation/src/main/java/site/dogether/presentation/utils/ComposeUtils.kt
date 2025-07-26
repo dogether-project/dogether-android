019a6c6f994c7e5648ffdd98faa69798934462dd
jangjh123 <gn0001231@gmail.com>
2025-07-27 00:28:00 +0900

    chore: ComposeUtils 추가
    Refs: #1

---
+++ presentation/src/main/java/site/dogether/presentation/utils/ComposeUtils.kt
@@ -0,0 +1,67 @@
+package site.dogether.presentation.utils
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalWindowInfo
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.compose.LocalLifecycleOwner
+
+@Composable
+fun Dp.toSp(): TextUnit = with(LocalDensity.current) { this@toSp.toSp() }
+
+@Composable
+fun Dp.toPx(): Float = with(LocalDensity.current) { this@toPx.toPx() }
+
+fun Modifier.clickableWithoutRipple(onClick: () -> Unit): Modifier = this.clickable(
+    interactionSource = MutableInteractionSource(),
+    indication = null
+) {
+    onClick()
+}
+
+fun Modifier.conditionedClickable(
+    condition: Boolean,
+    onClick: () -> Unit,
+): Modifier = if (condition) this.then(Modifier.clickable { onClick() }) else this
+
+fun Modifier.conditionedClickableWithoutRipple(
+    condition: Boolean,
+    onClick: () -> Unit,
+): Modifier = if (condition) this.then(Modifier.clickableWithoutRipple { onClick() }) else this
+
+@Composable
+fun Float.toDp() = with(LocalDensity.current) { this@toDp.toDp() }
+
+@Composable
+fun displayHeightRatio(ratio: Float): Dp = (LocalWindowInfo.current.containerSize.height * ratio).toDp()
+
+fun Color.alpha(alpha: Int) = this.copy(alpha = alpha / 100f)
+
+@Composable
+fun LifecycleEvent(
+    targetEvent: Lifecycle.Event,
+    onEvent: (Lifecycle.Event) -> Unit,
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == targetEvent) {
+                onEvent(event)
+            }
+        }
+
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+}
