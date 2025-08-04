ee7178532cc37add81e55e336e7242acad895b1f
jangjh123 <gn0001231@gmail.com>
2025-08-05 00:14:57 +0900

    feat: 그룹 생성 완료 UI 구현
    Related to: #12

--- presentation/src/main/java/site/dogether/presentation/AppNavGraph.kt
+++ presentation/src/main/java/site/dogether/presentation/AppNavGraph.kt
@@ -7,13 +7,14 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import site.dogether.presentation.screen.create_group.CreateGroupScreen
 import site.dogether.presentation.screen.force_update.ForceUpdateScreen
+import site.dogether.presentation.screen.group_created.GroupCreatedScreen
 import site.dogether.presentation.screen.on_boarding.OnBoardingScreen
 import site.dogether.presentation.screen.participation_method.ParticipationMethodScreen
 import site.dogether.presentation.screen.splash.SplashScreen
 
 @Composable
 fun AppNavGraph(
-    startDestination: String = Screen.CreateGroup.route,
+    startDestination: String = Screen.GroupCreated.route,
     navController: NavHostController = rememberNavController(),
 ) {
     NavHost(
@@ -25,6 +26,7 @@ fun AppNavGraph(
         composable(Screen.OnBoarding.route) { OnBoardingScreen() }
         composable(Screen.ParticipationMethod.route) { ParticipationMethodScreen() }
         composable(Screen.CreateGroup.route) { CreateGroupScreen() }
+        composable(Screen.GroupCreated.route) { GroupCreatedScreen() }
     }
 }
 
@@ -33,5 +35,6 @@ enum class Screen(val route: String) {
     ForceUpdate("force_update"),
     OnBoarding("on_boarding"),
     ParticipationMethod("participation_method"),
-    CreateGroup("create_group")
+    CreateGroup("create_group"),
+    GroupCreated("group_created")
 }
---
+++ presentation/src/main/java/site/dogether/presentation/screen/group_created/GroupCreatedScreen.kt
@@ -0,0 +1,142 @@
+package site.dogether.presentation.screen.group_created
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.koin.androidx.compose.koinViewModel
+import org.orbitmvi.orbit.compose.collectAsState
+import site.dogether.presentation.R
+import site.dogether.presentation.composables.CTAButton
+import site.dogether.presentation.theme.Body2_R
+import site.dogether.presentation.theme.ColorBgSurface
+import site.dogether.presentation.theme.ColorBorderDisabled
+import site.dogether.presentation.theme.ColorIconPrimary
+import site.dogether.presentation.theme.ColorIconSecondary
+import site.dogether.presentation.theme.ColorTextDefault
+import site.dogether.presentation.theme.ColorTextSecondary
+import site.dogether.presentation.theme.Head1_B
+
+@Composable
+fun GroupCreatedScreen(viewModel: GroupCreatedViewModel = koinViewModel()) {
+    GroupCreatedScreenContents()
+}
+
+@Composable
+private fun GroupCreatedScreenContents(viewModel: GroupCreatedViewModel = koinViewModel()) {
+    val uiState = viewModel.collectAsState().value
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .padding(top = 68.dp)
+                .fillMaxWidth()
+                .weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_congrats),
+                tint = ColorIconPrimary,
+                contentDescription = "icon_congrats"
+            )
+
+            Text(
+                modifier = Modifier.padding(top = 20.dp),
+                text = stringResource(R.string.title_group_created),
+                style = Head1_B,
+                textAlign = TextAlign.Center,
+                color = ColorTextDefault
+            )
+
+            Box(
+                modifier = Modifier
+                    .padding(
+                        top = 68.dp,
+                        start = (28.5).dp,
+                        end = (28.5).dp
+                    )
+                    .clip(RoundedCornerShape(12.dp))
+                    .fillMaxWidth()
+                    .height(84.dp)
+                    .border(
+                        width = 1.dp,
+                        shape = RoundedCornerShape(12.dp),
+                        color = ColorBorderDisabled
+                    )
+                    .background(ColorBgSurface)
+            ) {
+                Row(
+                    modifier = Modifier.align(Alignment.Center),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = uiState.code,
+                        style = Head1_B.copy(lineHeightStyle = LineHeightStyle.Default),
+                        color = ColorTextDefault
+                    )
+
+                    Icon(
+                        modifier = Modifier.padding(start = 8.dp),
+                        painter = painterResource(R.drawable.ic_export),
+                        tint = ColorIconSecondary,
+                        contentDescription = "icon_export"
+                    )
+                }
+            }
+
+            Row(
+                modifier = Modifier.padding(top = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    modifier = Modifier.size(16.dp),
+                    painter = painterResource(R.drawable.ic_notice_empty),
+                    tint = ColorIconSecondary,
+                    contentDescription = "icon_notice_empty"
+                )
+
+                Text(
+                    modifier = Modifier.padding(start = 4.dp),
+                    text = stringResource(R.string.notice_share),
+                    style = Body2_R.copy(lineHeightStyle = LineHeightStyle.Default),
+                    color = ColorTextSecondary
+                )
+            }
+        }
+
+        CTAButton(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .height(50.dp),
+            isEnabled = true,
+            text = stringResource(R.string.cta_button_navigate_to_home),
+            onClick = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun GroupCreatedScreenContentsPreview() {
+    GroupCreatedScreenContents()
+}
