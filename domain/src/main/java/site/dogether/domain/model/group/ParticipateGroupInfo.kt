78760e7a184dc5098207908369afd6746f5214dc
jangjh123 <gn0001231@gmail.com>
2026-03-13 22:07:24 +0900

    fix: 그룹 가입 요청 필드 수정 (#60)

--- domain/src/main/java/site/dogether/domain/model/group/ParticipateGroupInfo.kt
+++ domain/src/main/java/site/dogether/domain/model/group/ParticipateGroupInfo.kt
@@ -5,7 +5,7 @@ import site.dogether.domain.model.DomainModel
 data class ParticipateGroupInfo(
     val groupName: String,
     val duration: Int,
-    val maximumNumberCount: Int,
+    val maximumMemberCount: Int,
     val startAt: String,
     val endAt: String,
 ) : DomainModel
